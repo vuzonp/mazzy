b504b464948077992195e26fe0af8c6ea33b37a7
Thomas Girard <shrewstudio@gmail.com>
2014-04-14 20:26:01 +0200

    Création de la librairie gérant les contrôleurs

---
+++ src/Lib/Handler/Handler.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Thomas Girard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace Shrew\Mazzy\Lib\Handler;
+
+use Shrew\Mazzy\Lib\Security\Token;
+
+
+/**
+ * Application concrète de HandlerAbstract
+ *
+ * @author  Thomas Girard <thomas@shrewstudio.com>
+ * @license http://opensource.org/licenses/MIT
+ * @version v0.1.0-alpha2
+ * @since   2014-04-14
+ */
+class Handler extends HandlerAbstract
+{
+    
+    /**
+     * Génère un jeton de sécurité
+     * 
+     * @param string $label 
+     * @return string Le jeton généré
+     */
+    protected function generateToken($label)
+    {
+        $token = new Token($label);
+        return $token->generate();
+    }
+
+    /**
+     * Vérifie la validité d'un token
+     * 
+     * @param string $label Identifiant du token à comparer
+     * @param string $value Valeur du token reçu 
+     * @return boolean
+     */
+    protected function verifyToken($label, $value)
+    {
+        $token = new Token($label);
+        return $token->compare($value);
+    }
+    
+}
---
+++ src/Lib/Handler/HandlerException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Shrew\Framework\Lib;
+
+/**
+ * Exception consacrée aux erreurs de logique ou d'éxécution des composants
+ * de l'application (contrôleurs, modules, etc.)
+ * 
+ * @author Thomas Girard <thomas@shrewstudio.com>
+ * @package Lib
+ * @version 140330.20.58 alpha
+ */
+class HandlerException extends AppException
+{
+    
+}
